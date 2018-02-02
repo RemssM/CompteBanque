f38711f7f739e9313ee08980828c6f79e9c4fe75
Rémi Marcoux <36067771+RemssM@users.noreply.github.com>
2018-02-02 11:20:37 +0100

    ajout fonction

--- CompteBanque/src/banque/Compte.java
+++ CompteBanque/src/banque/Compte.java
@@ -1,5 +1,24 @@
 package banque;
 
 public class Compte {
-	private float solde;
+	private float solde = 0;
+
+	public void deposerArgent(float pArgent) {
+		solde += pArgent;
+	}
+
+	public float retirerArgent(float pArgent) {
+		float res = pArgent - solde;
+		solde -= pArgent;
+		return res;
+	}
+
+	public float virer(int pArgent, Compte compteDestination) {
+		float res = pArgent - solde;
+		solde -= pArgent;
+		compteDestination.deposerArgent(res);
+		return res;
+	}
+
 }
+
